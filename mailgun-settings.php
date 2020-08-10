3f2b8d6ee5aac4d0f28db5ef848097ab988d240f
Liran Markin <liran.markin@gmail.com>
2020-08-10 12:32:44 +0300

    Switched template to deha

---
+++ mailgun-settings.php
@@ -0,0 +1,24 @@
+<?php
+
+// Enter your Mailgun API key here
+/*
+|--------------------------------------------------------------------------
+| Mailgun Settings
+|--------------------------------------------------------------------------
+|
+| Register for Free on Mailgun.com for sending transactional emails.
+| Mailgun is powerful API email service by rackspace.
+| It is easy to use. Just signup on mailgun.com and
+| follow steps to get API key & domain name.
+|
+*/
+
+$mailgunKey = 'key-fd8ae0a70b4692a1346b6f833c4d6c21'; //Will be like key-........
+$mailgunDomain = 'sandboxb891b07a6bb446ba9f281ebe0d6ee0b8.mailgun.org'; //You can use sandbox domain provided by mailgun
+
+
+// Your details to receive email from end-user
+$yourEmail = 'liran.markin@gmail.com'; //Change this email id to yours where you want to receive email
+$yourMessage = 'Thank You! Your message has been sent.' //Change this mesage to show to sender on form
+
+?>
